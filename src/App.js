d7bf1590a6c4dee834825122cdbb3556f182d81d
HamdanAs <21hamdanas@gmail.com>
2021-06-15 12:56:07 +0700

    todo list

--- src/App.js
+++ src/App.js
@@ -1,64 +1,42 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           My name is Hamdan Abyadi Suwandi
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://instagram.com/hamdanasch"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           My Profile
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// event handler
-
-class Toggle extends Component{
-  constructor(props){
-    super(props)
-
-    this.state = {
-      toggleStatus: true
-    }
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(){
-    this.setState(state => ({
-      toggleStatus: !state.toggleStatus
-    }));
-  }
-
-  render(){
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.toggleStatus ? 'ON' : 'OFF'}
-      </button>
-    )
-  }
-}
+import List from './List';
 
 class App extends Component{
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      todoItem: '',
+      items: []
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      items: [...this.state.items, this.state.todoItem],
+      todoItem: ''
+    })
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      todoItem: event.target.value
+    });
+  }
+  
   render(){
     return (
-      <div className="App">
-        <Toggle />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.todoItem} onChange={this.handleChange} />
+          <button>Submit</button>
+        </form>
+
+        <List items={this.state.items} />
       </div>
     );
   }
