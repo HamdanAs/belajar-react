184b8b18e71a927738bb042054c2444f1b1f989f
HamdanAs <21hamdanas@gmail.com>
2021-06-15 13:43:41 +0700

    React Fragment

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
-
-function Home(){
-  return <h2>Ini adalah halaman Home</h2>
-}
-
-function User(){
-  return (
-    <div>
-      <h2>Ini adalah halaman User</h2>
-      <ul>
-        <li><Link to="/user/hamdan">Hamdan</Link></li>
-        <li><Link to="/user/abyadi">Abyadi</Link></li>
-      </ul>
-    </div>
-  );
-}
-
-function DetailUser({ match }){
-  return <h2>Ini adalah halaman {match.params.name}</h2>
-}
-
-function NotFound(){
-  return <h2>Halaman tidak ditemukan</h2>
-}
 
 class App extends Component{
 
   render(){
 
     return (
-      <BrowserRouter>
-        <div>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/users">User</Link></li>
-            </ul>
-          </nav>
+      <div>
+        <table>
+          <tr>
+            <Column />
+          </tr>
+        </table>
+      </div>
+    );
+  }
+}
 
-          <main>
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/users" exact component={User} />
-              <Route path="/user/:name" exact component={DetailUser} />
-              <Route component={NotFound} />
-            </Switch>
-          </main>
-        </div>
-      </BrowserRouter>
+class Column extends React.Component{
+  render(){
+    return (
+      <React.Fragment>
+        <td>Hamdan</td>
+        <td>Abyadi</td>
+      </React.Fragment>
     );
   }
 }
