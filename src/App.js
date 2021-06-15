fe40e5b5da6e0679c2c075529d8b85e165e8cd27
HamdanAs <21hamdanas@gmail.com>
2021-06-15 13:39:04 +0700

    react router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
 import React, {Component} from 'react';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
 
+function Home(){
+  return <h2>Ini adalah halaman Home</h2>
+}
+
+function User(){
+  return (
+    <div>
+      <h2>Ini adalah halaman User</h2>
+      <ul>
+        <li><Link to="/user/hamdan">Hamdan</Link></li>
+        <li><Link to="/user/abyadi">Abyadi</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+function DetailUser({ match }){
+  return <h2>Ini adalah halaman {match.params.name}</h2>
+}
+
+function NotFound(){
+  return <h2>Halaman tidak ditemukan</h2>
+}
+
 class App extends Component{
-  
-  // API
-
-  constructor(props){
-    super(props);
-
-    this.state = {
-      items: [],
-      isLoading: true
-    }
-  }
-
-  componentDidMount(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(data => this.setState({ items: data, isLoading: false }));
-  }
 
   render(){
 
-    const { items, isLoading } = this.state
-
-    if(isLoading){
-      return <p>Loading ...</p>
-    }
-
     return (
-      <div>
-        <ul>
-          {
-            items.map((item, index) => <li key={index}>{item.name}</li>)
-          }
-        </ul>
-      </div>
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/users">User</Link></li>
+            </ul>
+          </nav>
+
+          <main>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/users" exact component={User} />
+              <Route path="/user/:name" exact component={DetailUser} />
+              <Route component={NotFound} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
     );
   }
 }
