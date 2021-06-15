ff9a70fd63a07a01ea615dba4bf9b35c40e6e170
HamdanAs <21hamdanas@gmail.com>
2021-06-15 11:31:47 +0700

    pengenalan jsx

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Hamdan Abyadi Suwandi
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://instagram.com/hamdanasch"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Profile
         </a>
       </header>
     </div>
